f82d1fd0b3a2c489716b0e773b0f2b2c3a94b62a
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-06-04 06:38:36 +0100

    initial dashboard and SignUp component

---
+++ src/components/auth/SignUp.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react'
+
+
+class SignUp extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                
+            </div>
+         );
+    }
+}
+ 
+export default SignUp;
---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SignUp from '../auth/SignUp';
+class Dashboard extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <div>
+                <SignUp />
+            </div>
+        );
+    }
+}
+ 
+export default Dashboard;
