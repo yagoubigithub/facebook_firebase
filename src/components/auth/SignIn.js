a15587f0c5a3348a0a8cb053069d63c20ab7d4d0
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-06-04 05:06:46 +0100

    add grid system to nabar and signIn

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,17 +1,27 @@
-import React, { Component } from 'react';
- 
+import React, { Component } from "react";
+import { Grid } from "@material-ui/core";
 
 class SignIn extends Component {
-    state = {  }
-    render() { 
-        return (  <React.Fragment>
-            <form>
-                <input type="email" name="email" />
-                <input type="password" name="password" />
-                <button >Connexion</button>
-            </form>
-        </React.Fragment>);
-    }
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <form>
+          <Grid container>
+            <Grid item xs={4}>
+              <input type="email" name="email" />
+            </Grid>
+            <Grid item xs={4}>
+              <input type="password" name="password" />
+            </Grid>
+            <Grid item xs={4}>
+              <button>Connexion</button>
+            </Grid>
+          </Grid>
+        </form>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default SignIn;
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
-import SignIn from '../auth/SignIn'; 
-
+import React, { Component } from "react";
+import SignIn from "../auth/SignIn";
+import { Grid ,Hidden } from "@material-ui/core";
 class Navbar extends Component {
-    state = {  }
-    render() { 
-        return (  <nav className="navbar">
-        <h1>Facebook</h1>
-        <SignIn />
-
-        </nav>);
-    }
+  state = {};
+  render() {
+    return (
+      <nav className="navbar">
+        <Grid container>
+        <Hidden xsDown>
+        <Grid item md={7} sm={4}>
+            <h1>Facebook</h1>
+          </Grid>
+        </Hidden>
+         
+          <Grid item>
+            <SignIn />
+          </Grid>
+        </Grid>
+      </nav>
+    );
+  }
 }
- 
+
 export default Navbar;
